8684f8cc7787e03665b10a0565feda7fd4195801
hiren bista <119731447+streeter-phelps@users.noreply.github.com>
2024-06-17 22:29:14 +0545

    Update numb3rs.py

--- Week7/numb3rs/numb3rs.py
+++ Week7/numb3rs/numb3rs.py
@@ -8,7 +8,8 @@ def main():
 def validate(ip):
     if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
         return False
-    for num in ip.split("."):
+    nums = ip.split(".") # just for readability for beginners
+    for num in nums:
         if int(num) < 0 or int(num) > 255:
             return False
     return True
